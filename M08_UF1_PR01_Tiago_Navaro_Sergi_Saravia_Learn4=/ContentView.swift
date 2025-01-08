4aec9ad1484e41e228657acb8d9ec2e88d1d0cb6
Pau_Marcel <marcel.ggda@gmail.com>
2025-01-08 18:47:22 +0100

    Initial Commit

---
+++ M08_UF1_PR01_Tiago_Navaro_Sergi_Saravia_Learn4=/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M08_UF1_PR01_Tiago_Navaro_Sergi_Saravia_Learn4=
+//
+//  Created by alumne on 08/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
