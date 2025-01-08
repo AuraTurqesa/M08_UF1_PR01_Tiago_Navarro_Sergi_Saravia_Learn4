d5ddf11c9a2da91393e252828914fd0bf1797671
tiagunskiii28 <tiagunskiii@gmail.com>
2025-01-08 19:15:24 +0100

    Initial Commit

---
+++ trivia/triviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  triviaApp.swift
+//  trivia
+//
+//  Created by alumne on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct triviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
