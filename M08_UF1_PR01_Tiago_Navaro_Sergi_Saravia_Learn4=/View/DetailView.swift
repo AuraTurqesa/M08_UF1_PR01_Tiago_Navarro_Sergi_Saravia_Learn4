b9d13e1e3a5a6984e3a63e0b98abefb01a110374
tiagunskiii28 <tiagunskiii@gmail.com>
2025-01-20 15:34:35 +0100

    Icono añadido

--- M08_UF1_PR01_Tiago_Navaro_Sergi_Saravia_Learn4=/View/DetailView.swift
+++ M08_UF1_PR01_Tiago_Navaro_Sergi_Saravia_Learn4=/View/DetailView.swift
@@ -1,36 +1,70 @@
-
 import SwiftUI
 
 struct DetailView: View {
     var trivia: Trivia  // Recibimos un objeto `Trivia`
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Pregunta: \(trivia.pregunta.text)")
-                .font(.title)
-                .padding(.bottom)
+        GeometryReader {_ in
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Pregunta: \(trivia.pregunta.text)")
+                        .font(.title)
+                        .padding(.bottom)
 
-            Text("Categoría: \(trivia.categoria)")
-                .font(.subheadline)
-                .padding(.bottom)
+                    Text("Categoría: \(trivia.categoria)")
+                        .font(.subheadline)
+                        .padding(.bottom)
 
-            Text("Dificultad: \(trivia.dificultat)")
-                .font(.subheadline)
-                .padding(.bottom)
+                    Text("Dificultad: \(trivia.dificultat)")
+                        .font(.subheadline)
+                        .padding(.bottom)
 
-            Text("Respuestas Incorrectas:")
-                .font(.subheadline)
-                .padding(.top)
-            
-            ForEach(trivia.respostesIncorrectes, id: \.self) { respuesta in
-                Text("- \(respuesta)")
-                    .font(.body)
-                    .padding(.leading)
+                    Text("Tipo: \(trivia.tipus)")
+                        .font(.subheadline)
+                        .padding(.bottom)
+
+                    Text("Es Nicho: \(trivia.esNiche ? "Sí" : "No")")
+                        .font(.subheadline)
+                        .padding(.bottom)
+
+                    Text("Respuestas Correctas:")
+                        .font(.headline)
+                        .padding(.top)
+
+                    Text("Correcta: \(trivia.respostaCorrecta)")
+                        .font(.body)
+                        .padding(.bottom)
+
+                    Text("Respuestas Incorrectas:")
+                        .font(.headline)
+
+                    ForEach(trivia.respostesIncorrectes, id: \.self) { respuesta in
+                        Text("- \(respuesta)")
+                            .font(.body)
+                            .padding(.leading)
+                    }
+
+                    
+
+                    if trivia.regions.isEmpty {
+                        Text("No hay regiones asignadas")
+                            .font(.body)
+                    } else {
+                        Text("Regiones:")
+                            .font(.headline)
+                            .padding(.top)
+                        ForEach(trivia.regions, id: \.self) { region in
+                            Text("- \(region)")
+                                .font(.body)
+                                .padding(.leading)
+                        }
+                    }
+                
+                }
+                .padding()
             }
-
-            Spacer()
         }
-        .padding()
+            
         .navigationBarTitle("Detalles de la Pregunta", displayMode: .inline)
     }
 }
